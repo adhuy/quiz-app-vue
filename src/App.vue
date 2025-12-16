1df3763748582941733a13b81701c65cbcccd62d
Adhuy <yudhamerdeka91@gmail.com>
2025-12-16 15:15:54 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-50">
+    <div class="w-full max-w-xl p-6 bg-gray-200 rounded-2xl border drop-shadow-[4px_4px_0px_#000]">
+      <HelloWorld msg="Welcome to Quiz App" />
+    </div>
+  </div>
+</template>
